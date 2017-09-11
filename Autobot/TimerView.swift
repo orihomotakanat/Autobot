aee90a1e794c960ff62233c34501cf93a9bf4ede
Tomohiro TANAKA <tanaka1201t@gmail.com>
2017-09-11 16:11:18 +0900

    Add switch icon

---
+++ Autobot/TimerView.swift
@@ -0,0 +1,21 @@
+//
+//  TimerView.swift
+//  Autobot
+//
+//  Created by Tanaka, Tomohiro on 2017/09/11.
+//  Copyright © 2017年 Tanaka, Tomohiro. All rights reserved.
+//
+
+import UIKit
+
+class TimerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
