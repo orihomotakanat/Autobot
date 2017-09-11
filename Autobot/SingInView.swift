ba39e88b4bc6b15f4663d53b92b6013ff3f2a793
Tomohiro TANAKA <tanaka1201t@gmail.com>
2017-09-11 22:08:51 +0900

    Modification of SignUp code

--- Autobot/SingInView.swift
+++ Autobot/SingInView.swift
@@ -7,9 +7,36 @@
 //
 
 import UIKit
+import AWSCognitoIdentityProvider
 
 class SingInView: UIViewController {
-
+    
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var passWord: UITextField!
+    var pwAuthCompletion: AWSTaskCompletionSource<AWSCognitoIdentityPasswordAuthenticationDetails>?
+    var userNameText: String?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.userName.text = userNameText //Username
+        self.passWord.text = nil //Password
+    }
+    
+    //When pushing "Sign In"
+    @IBAction func signInPressed(_ sender: Any) {
+        if (self.userName.text != nil && self.passWord.text != nil) { //Username&Password are NOT nil
+            let authDetails = AWSCognitoIdentityPasswordAuthenticationDetails(username: self.userName.text!, password: self.passWord.text!) //username&passwordの入力値をauthDetailsへ格納
+            self.pwAuthCompletion?.set(result: authDetails) //authDetailsをCognitioUserPoolに登録する用に格納
+        } else { //UsernameまたはPasswordがnilの場合
+            let alertController = UIAlertController(title: "Missing information",
+                                                    message: "Please enter a valid username and password",
+                                                    preferredStyle: .alert)
+            let retryAction = UIAlertAction(title: "Retry", style: .default, handler: nil)
+            alertController.addAction(retryAction) //AlertViewを上記内容で表示する
+        }
+    } //@IBAction func siginInPressed end
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,5 +58,32 @@ class SingInView: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
+
+extension SingInView: AWSCognitoIdentityPasswordAuthentication {
+    public func getDetails(_ authenticationInput: AWSCognitoIdentityPasswordAuthenticationInput, passwordAuthenticationCompletionSource: AWSTaskCompletionSource<AWSCognitoIdentityPasswordAuthenticationDetails>) {
+        self.pwAuthCompletion = passwordAuthenticationCompletionSource
+        DispatchQueue.main.async {
+            if (self.userNameText == nil) {
+                self.userNameText = authenticationInput.lastKnownUsername
+            }
+        }
+    } //public func getDetails end
+    
+    public func didCompleteStepWithError(_ error: Error?) {
+        DispatchQueue.main.async {
+            if let error = error as NSError? {
+                let alertController = UIAlertController(title: error.userInfo["__type"] as? String,
+                                                        message: error.userInfo["message"] as? String,
+                                                        preferredStyle: .alert)
+                let retryAction = UIAlertAction(title: "Retry", style: .default, handler: nil)
+                alertController.addAction(retryAction)
+                
+                self.present(alertController, animated: true, completion:  nil)
+            } else {
+                self.userName.text = nil
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
+    }//public func didCompleteStepWithError end
+} //extension SingInView end
