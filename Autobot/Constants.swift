8181e9ff628a827078e37be1147cb472b16862e6
Orihomotakanat <tanakat1201t@gmail.com>
2017-09-11 10:31:46 +0900

    Add constants for CognitoID

---
+++ Autobot/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Autobot
+//
+//  Created by 田中智大 on 2017/09/11.
+//  Copyright © 2017年 Tanaka, Tomohiro. All rights reserved.
+//
+
+import Foundation
+import AWSCognitoIdentityProvider
+
+let CognitoIdentityUserPoolRegion: AWSRegionType = ".REGION" //(ex: .USEast1)
+let CognitoIdentityUserPoolId = "YOUR_USER_POOL_ID"
+let CognitoIdentityUserPoolAppClientId = "YOUR_APP_CLIENT_ID"
+let CognitoIdentityUserPoolAppClientSecret = "YOUR_APP_CLIENT_SECRET"
+
+let AWSCognitoUserPoolsSignInProviderKey = "UserPool"
