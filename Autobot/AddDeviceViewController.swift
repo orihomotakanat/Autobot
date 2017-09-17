65ab861ec6b2e9cbe65bd4bb8d5ed160bf3569f1
Orihomotakanat <tanakat1201t@gmail.com>
2017-09-17 23:02:36 +0900

    Insert data to DDB

--- Autobot/AddDeviceViewController.swift
+++ Autobot/AddDeviceViewController.swift
@@ -15,19 +15,29 @@ class AddDeviceViewController: UIViewController, UIPickerViewDelegate, UIPickerV
     @IBOutlet weak var roomName: UITextField!
     @IBOutlet var deviceSelectPicker: UIPickerView!
     
-    //cognitoCredentials
+    //cognitoCredentials for inserting to DynamoDB
     var response: AWSCognitoIdentityUserGetDetailsResponse?
     var user: AWSCognitoIdentityUser?
     var pool: AWSCognitoIdentityUserPool?
     
     let pickerViewList: Array = ["RaspberryPi3"] //deviceの機種
-    //let controlPickerList: Array = ["Air conditionar"] //aircon, TVremote
-
+    let temporaryMethod: String = "aircon" //Method
+    
+    //For insertToDDB
+    var tableRow: DDBTableRow?
+    //var dataChanged = false //Update機能は後に
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        // - CognitoUserPool
+        self.pool = AWSCognitoIdentityUserPool(forKey: AWSCognitoUserPoolsSignInProviderKey)
+        if (self.user == nil) {
+            self.user = self.pool?.currentUser()
+        }
+        
         // - deviceSelectPicker
         deviceSelectPicker.setValue(UIColor.white, forKey: "textColor")
         deviceSelectPicker.backgroundColor = #colorLiteral(red: 0.001564681064, green: 0.05989853293, blue: 0.1638002098, alpha: 1)
@@ -37,6 +47,53 @@ class AddDeviceViewController: UIViewController, UIPickerViewDelegate, UIPickerV
         
         roomName.delegate = self
     }
+    
+    //Save device setting to DDB
+    @IBAction func insertToDDB(_ sender: Any) {
+        let tableRow = DDBTableRow()
+        tableRow?.username = (self.user?.username)!
+        tableRow?.roomname = self.roomName.text
+        tableRow?.device = pickerViewList[0]
+        tableRow?.method = temporaryMethod
+        if (self.roomName.text!.utf16.count > 0) {
+            self.insertTableRow(tableRow!)
+        } else {
+            let alertController = UIAlertController(title: "Error: Invalid Input", message: "Range Key Value cannot be empty.", preferredStyle: UIAlertControllerStyle.alert)
+            let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        
+    }
+    
+    //insert to DDB funcation
+    func insertTableRow(_ tableRow: DDBTableRow) {
+        let dynamoDBObjectMapper = AWSDynamoDBObjectMapper.default()
+        
+        dynamoDBObjectMapper.save(tableRow) .continueWith(executor: AWSExecutor.mainThread(), block: { (task:AWSTask!) -> AnyObject! in
+            if let error = task.error as NSError? {
+                print("Error: \(error)")
+                
+                let alertController = UIAlertController(title: "Failed to insert the data into the table.", message: error.description, preferredStyle: UIAlertControllerStyle.alert)
+                let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+                alertController.addAction(okAction)
+                self.present(alertController, animated: true, completion: nil)
+            } else { //Fail to insertでない場合
+                let alertController = UIAlertController(title: "Succeeded", message: "Successfully registered your device", preferredStyle: UIAlertControllerStyle.alert)
+                let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+                alertController.addAction(okAction)
+                self.present(alertController, animated: true, completion: nil)
+                
+                self.roomName.text = nil
+
+                //self.dataChanged = true //Update機能は後に
+            }
+            
+            return nil
+        })
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
