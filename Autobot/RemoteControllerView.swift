1f7807fc2821d369b01c922d1ebb2f07e0412d33
Orihomotakanat <tanakat1201t@gmail.com>
2017-09-20 05:07:46 +0900

    Add Time view

--- Autobot/RemoteControllerView.swift
+++ Autobot/RemoteControllerView.swift
@@ -25,12 +25,36 @@ class RemoteControllerView: UIViewController {
     //AWSIoT
     var iotDataManager: AWSIoTDataManager!
     
+    //Timer
+    @IBOutlet var Hour: UILabel!
+    @IBOutlet var Minute: UILabel!
+    @IBOutlet var Second: UILabel!
+    @IBOutlet var colonHourAndMinute: UILabel!
+    @IBOutlet var colonMinutesAndSecond: UILabel!
+    
+    var counter: Int = 0 //タイマーのカウント
+    var internalHour: Int = 0 //Hour
+    var internalMin: Int = 0 //Min
+    var internalSec: Int = 0 //Second
+    
+    var presentTime = Timer()
+    var reservedConfirmation = true //trueの時1回目のカウントの時
+    var alreadyStartedTimer = true //trueの時はまだOnになってないorOFFが押された時、falseの時はTimerが動いている時
+    var timerIdentifier = true
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //Preparation of tapticEngine
         tapticGenerator.prepare()
         tapticNotficationGenerator.prepare() //tapticEngine for timer
+        
+        //CoundDownTimer: default -> invisible
+        Hour.isHidden = true
+        Minute.isHidden = true
+        Second.isHidden = true
+        colonHourAndMinute.isHidden = true
+        colonMinutesAndSecond.isHidden = true
                 
         // Do any additional setup after loading the view.
         reservedTimerPicker.setValue(UIColor.white, forKey: "textColor")
@@ -63,30 +87,109 @@ class RemoteControllerView: UIViewController {
         print("Turn on")
         iotDataManager.publishString("turn on", onTopic: publishTopic, qoS:.messageDeliveryAttemptedAtMostOnce)
         tapticGenerator.impactOccurred()
+
     }
     
     
     //When pushing reserve button
     @IBAction func pushedReserve(_ sender: Any) {
-        //処理
-        turnedOn()
+        //処理 //ここだけtimer側にturndOn()を記載
+        if alreadyStartedTimer {
+            presentTime = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.passed), userInfo: nil, repeats: true)
+            alreadyStartedTimer = false
+        }
         
     }
     
     //When pusing on button
     @IBAction func pushedOn(_ sender: Any) {
         //処理
+        //CoundDownTimer: turnOn -> invisible
+        reservedTimerPicker.isHidden = true
+        Hour.isHidden = true
+        Minute.isHidden = true
+        Second.isHidden = true
+        colonHourAndMinute.isHidden = true
+        colonMinutesAndSecond.isHidden = true
+        
+        //TurnOn
         turnedOn()
+        
+        reservedConfirmation = false
+        presentTime.invalidate()
     }
     
     //When pushing off button
     @IBAction func pushedOff(_ sender: Any) {
+        
+        //CoundDownTimer: turnOff -> pickerView; visible
+        reservedTimerPicker.isHidden = false
+        Hour.isHidden = true
+        Minute.isHidden = true
+        Second.isHidden = true
+        colonHourAndMinute.isHidden = true
+        colonMinutesAndSecond.isHidden = true
+        
+        presentTime.invalidate()
+        reservedConfirmation = true //reserveされていない状態に戻す
+        alreadyStartedTimer = true //timerが作動していない状態に戻す
+        
         //処理
         let publishTopic = "\(thingName)/control/off"
         print("Turn off")
         iotDataManager.publishString("turn on", onTopic: publishTopic, qoS:.messageDeliveryAttemptedAtMostOnce)
         tapticGenerator.impactOccurred()
     }
+    
+    //reserve Timer用の設定
+    func passed(timeCount: Timer){
+        if reservedConfirmation {
+            counter = Int(reservedTimerPicker.countDownDuration)
+            
+            //CoundDownTimer: reserved -> pickerView; invisible
+            reservedTimerPicker.isHidden = true
+            Hour.isHidden = false
+            Minute.isHidden = false
+            Second.isHidden = false
+            colonHourAndMinute.isHidden = false
+            colonMinutesAndSecond.isHidden = false
+            
+            reservedConfirmation = false
+        }
+        
+        internalHour = Int(counter/3600) //Hour
+        internalMin = Int((counter - internalHour * 3600) / 60) //Min
+        internalSec = Int(counter - internalHour*3600 - internalMin*60) //sec
+        counter = counter - 1
+        //Timer設定
+        if counter == 0 {
+            //ここにAirconの機能を入れる
+            turnedOn()
+            
+            tapticNotficationGenerator.notificationOccurred(.success)
+            presentTime.invalidate() //Timer停止
+            reservedConfirmation = true //reserveされていない状態に戻す
+            alreadyStartedTimer = true //timerが作動していない状態に戻す
+            
+            //ON表示
+            Hour.isHidden = true
+            Minute.isHidden = true
+            Second.isHidden = true
+            colonHourAndMinute.isHidden = true
+            colonMinutesAndSecond.isHidden = true
+        
+            print("Timer is stopped & Start Aircon")
+            
+        } else {
+            Hour.text = String(format: "%02d", internalHour) //タイマーのカウントする
+            Minute.text = String(format: "%02d", internalMin)
+            Second.text = String(format: "%02d", internalSec)
+            print(counter) //For debug
+            
+        }
+    }
+    
+    
     
     //MQTT callback
     func mqttEventCallback( _ status: AWSIoTMQTTStatus ) {
@@ -102,7 +205,6 @@ class RemoteControllerView: UIViewController {
                 //
                 // Register the device shadows once connected.
                 //
-                
                 self.iotDataManager.getShadow(self.thingName)
                 
             case .disconnected:
